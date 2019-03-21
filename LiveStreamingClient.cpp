204f2e1ea3d5f5cd0dcbd242ffe0131bbce682cf
Jacky Chan <chankongching@gmail.com>
2019-03-21 15:15:23 +0800

    Fix syntax error

--- LiveStreamingClient.cpp
+++ LiveStreamingClient.cpp
@@ -18,7 +18,7 @@ char products [][7] = {"AUD.CAD", "AUD.CHF", "AUD.CNH", "AUD.HKD", "AUD.JPY", "A
             "KRW.HKD", "KRW.JPY", "KRW.USD", "MXN.JPY", "NOK.JPY", "NOK.SEK", "NZD.CAD", "NZD.CHF", "NZD.JPY",
             "NZD.USD", "SEK.JPY", "SGD.CNH", "SGD.JPY", "TRY.JPY", "USD.CAD", "USD.CHF", "USD.CNH", "USD.CZK",
             "USD.DKK", "USD.HKD", "USD.HUF", "USD.ILS", "USD.JPY", "USD.KRW", "USD.MXN", "USD.NOK", "USD.PLN",
-            "USD.RUB", "USD.SEK", "USD.SGD", "USD.TRY", "USD.ZAR", "ZAR.JPY"}
+            "USD.RUB", "USD.SEK", "USD.SGD", "USD.TRY", "USD.ZAR", "ZAR.JPY"};
 
 
 // member funcs
@@ -80,7 +80,7 @@ void LiveSteamingClient::getMarketData()
 
 void LiveSteamingClient::subscribe_all_contracts_lob(){
   printf("Product size = ");
-  printf(products.size());
+  printf(sizeof(products[0]));
 }
 // Inheriting old program
 void LiveSteamingClient::orderStatus( OrderId orderId, const IBString &status, int filled,
